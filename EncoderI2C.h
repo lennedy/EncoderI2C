99137785dc087c847651dbac7b6f3a66e73e8f3d
lennedy <lennedy@gmail.com>
2018-09-26 12:59:53 -0300

    Remoção de arquvivo
    Unificação da classe Conversão.h dentro da classe EncoderI2C.h com a consequente remoção do arquivo Conversao.h

--- EncoderI2C.h
+++ EncoderI2C.h
@@ -5,8 +5,38 @@
 //	#include <TinyWireS.h>
 //#else
 #include <Wire.h>
-#include <Conversao.h>
+//#include <Conversao.h>
 //#endif
+
+#define DATA_SIZE 4 
+
+class Conversao{
+  public:
+  long arrayToLong(const uint8_t* c){
+    unsigned long temp=0;
+ 		for(int i=DATA_SIZE-1; i>=0; i--){
+		  temp+=(unsigned long)c[i];
+			if(i>0)
+			  temp<<=8;  
+		}
+//  temp = (long)c[0];
+    return (long)temp;
+  }
+
+  void longToArray(const long t, uint8_t* c){
+    c[0] = t>>24;
+    c[1] = t>>16;
+    c[2] = t>>8;
+    c[3] = t & 0xff;
+  }
+  
+  void intToChar(const int t, uint8_t* c){
+    c[0] = t>>8;
+    c[1] = t & 0xff;
+  }
+
+};
+
 
 class EncoderI2C{
 	public:
